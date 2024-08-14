095addb6c553efecac06897472e3e48edfefd9ec
jsilveira1409 <joaquim.silveira@epfl.ch>
2024-08-14 09:17:42 +0200

    feat: implemented configure script

--- configure.sh
+++ configure.sh
@@ -1,5 +1,21 @@
-echo "Setting up daemon.json for docker"
-cp ./deploy/docker-daemon.json /etc/docker/daemon.json
+#!/bin/bash
+
+# Request sudo permissions at the beginning
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root or with sudo."
+  exit
+fi
+
+echo "Setting up daemon.json for Docker"
+cp ./docker-daemon.json /etc/docker/daemon.json
 systemctl restart docker.service
+
 echo "Setting up registry hostname"
-echo "0.0.0.0    registry" | sudo tee -a /etc/hosts > /dev/null
+
+# Check if "0.0.0.0 registry" already exists in /etc/hosts
+if ! grep -q "^0.0.0.0[[:space:]]\+registry" /etc/hosts; then
+  echo "0.0.0.0    registry" | tee -a /etc/hosts > /dev/null
+  echo "Added '0.0.0.0 registry' to /etc/hosts"
+else
+  echo "'0.0.0.0 registry' already exists in /etc/hosts"
+fi
